74de3a5f9962222b854f254df3efa71d1c8dd9bb
charlesalexandree <167128257+charlesalexandree@users.noreply.github.com>
2024-04-15 15:51:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -51,27 +51,7 @@
 
 	<div class="cards">
 
-	<?php
-	foreach($servidores as $servidor => $ip)
-	{
-		// $retorno = shell_exec("C:\Windows\system32\ping -n 1 $ip"); - Este codigo não funciona em servidor compartilhado por questões de segurança.
-		$retorno = @fsockopen($ip,80,$errCode,$errStr,2);
-		
-		// if (preg_match("/tempo</", $retorno) || preg_match("/tempo=/", $retorno))  - Este codigo não funciona em servidor compartilhado por questões de segurança.
-		if ($retorno)
-		{
-			$status = "online";
-		} else {
-			$status = "offline";
-		}
-	?>
-		<div class="card <?=$status?>">
-			<div class="servidor"><?=$servidor?></div>
-			<div class="ip"><?=$ip?></div>
-			<div class="status"><?=$status?></div>
-		</div>
-
-	<?php
+	
 	}
 	?>
 
